eaf09f23622de55a03d105c71d7bf1cbf26554ec
Andy Terwilliger <76022623+andyterwilliger@users.noreply.github.com>
2022-03-04 10:10:58 -0600

    DB migrated DB migrated to include Link model

--- models/link.js
+++ models/link.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Link extends Model {
     /**
@@ -13,12 +11,21 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Link.init({
-    url: DataTypes.STRING,
-    slug: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Link',
-  });
+  Link.init(
+    {
+      url: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      slug: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    {
+      sequelize,
+      modelName: "Link"
+    }
+  );
   return Link;
 };
