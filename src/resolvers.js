53110172afbef39d20cd91874e8507c777193a40
Andy Terwilliger <76022623+andyterwilliger@users.noreply.github.com>
2022-03-04 10:24:05 -0600

    Resolvers added to link added Query, mutation resolvers for link model

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,3 +1,5 @@
+const link = require("../models/link");
+
 const resolvers = {
   Query: {
     async allDemos(root, args, { models }) {
@@ -5,11 +7,20 @@ const resolvers = {
     },
     async demo(root, { id }, { models }) {
       return models.Demo.findByPk(id);
+    },
+    async link(root, { id }, { models }) {
+      return models.Link.findByPk(id);
+    },
+    async allLinks(root, args, { models }) {
+      return models.Link.findAll();
     }
   },
   Mutation: {
     createDemo(root, { name, text }, { models }) {
       return models.Demo.create({ name, text });
+    },
+    createLink(root, { url, slug }, { models }) {
+      return models.Link.create({ url, slug });
     }
   }
 };
